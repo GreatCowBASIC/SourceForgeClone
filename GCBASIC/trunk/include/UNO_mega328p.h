b9d461cff4be7ce6b607292da815e66611815ffb
Anobium <17933379+Anobium@users.noreply.github.com>
2015-03-16 15:17:16 +0000

    New UNO UNO_mega328p.h file

---
+++ GCBASIC/trunk/include/UNO_mega328p.h
@@ -0,0 +1,26 @@
+'UNO - mega328p
+
+#define D0 portD.0
+#define D1 portD.1
+#define D2 portD.2
+#define D3 portD.3
+#define D4 portD.4
+#define D5 portD.5
+#define D6 portD.6
+#define D7 portD.7
+#define D8 portb.0
+#define D9 portb.1
+#define D10 portb.2
+#define D11 portb.3
+#define D12 portb.4
+#define D13 portb.5
+
+#define A0 portC.0
+#define A1 portC.1
+#define A2 portC.2
+#define A3 portC.3
+#define A4 portC.4
+#define A5 portC.5
+
+#define RESET portc.6
+
