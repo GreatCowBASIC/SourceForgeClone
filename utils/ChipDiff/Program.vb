7ddda0719afdb6268e6cf764922b5786b7512273
w_cholmondeley <w_cholmondeley@8d896844-3110-41cb-9cb2-732a89be98bc>
2015-07-04 17:25:55 +0000

    Improved chip data comparison utility. Now checks memory ranges and configuration settings. Also has a mode to ignore added features in the second folder, and only show changes from 1 to 2 that are likely to cause issues.

--- utils/ChipDiff/Program.vb
+++ utils/ChipDiff/Program.vb
@@ -27,6 +27,8 @@ Imports System.IO
 Imports System.Linq
 
 Module Program
+	Dim Update As Boolean = False
+	
 	Sub Main()
 		
 		Dim SourceDir1, SourceDir2 As String
@@ -35,17 +37,21 @@ Module Program
 		SourceDir2 = "C:\Final\New Chip Data\chipdata\"
 		
 		'Get command line arguments
-		If My.Application.CommandLineArgs.Count <> 2 Then
+		If My.Application.CommandLineArgs.Count < 2 Or My.Application.CommandLineArgs.Count > 3 Then
 			Console.WriteLine("ChipDiff - compares two chip data file folders")
 			Console.WriteLine()
 			Console.WriteLine("Syntax:")
-			Console.WriteLine("       ChipDiff folder1 folder2")
+			Console.WriteLine("       ChipDiff folder1 folder2 [/update]")
 			Console.WriteLine()
 			Console.WriteLine("where folder1 and folder2 are paths to folders containing the .dat files")
+			Console.WriteLine("      /update indicates that folder2 has newer files - ignore additions")
 			Exit Sub
 		Else
 			SourceDir1 = My.Application.CommandLineArgs(0)
 			SourceDir2 = My.Application.CommandLineArgs(1)
+			If My.Application.CommandLineArgs.Count = 3 Then
+				Update = My.Application.CommandLineArgs(2).ToLower = "/update"
+			End If
 		End If
 		
 		'Create lists
@@ -76,10 +82,28 @@ Module Program
 			Else If Not Files2.ContainsKey(CurrChip) Then
 				Console.WriteLine("Only in 1:" + CurrChip)
 			Else
-				'Chip in both lists, compare
+				'Chip is in both lists, compare
 				Dim Chip1, Chip2 As ChipInfo
+				
+				'Get data for chip 1, check for loading issues
 				Chip1 = New ChipInfo(SourceDir1, CurrChip)
+				If Not Update Then
+					If Chip1.ChipNotValid Then
+						Console.WriteLine(Chip1.ChipName + ": Chip 1 not valid")
+					Else If Chip1.ChipSuspect Then
+						Console.WriteLine(Chip1.ChipName + ": Chip 1 is suspect")
+					End If
+				End If
+				
+				'Get data for chip 2, check for loading issues
 				Chip2 = New ChipInfo(SourceDir2, CurrChip)
+				If Chip2.ChipNotValid Then
+					Console.WriteLine(Chip2.ChipName + ": Chip 2 not valid")
+				Else If Chip2.ChipSuspect Then
+					Console.WriteLine(Chip2.ChipName + ": Chip 2 is suspect")
+				End If
+				
+				'Compare chips
 				CompareChips(Chip1, Chip2)
 				
 			End If
@@ -90,46 +114,47 @@ Module Program
 	End Sub
 	
 	Sub CompareChips(Chip1 As ChipInfo, Chip2 As ChipInfo)
+		
 		'Check basic info
 		If Chip1.ChipProgMem <> Chip2.ChipProgMem Then
-			Console.WriteLine(Chip1.ChipName + ":Prog memory differs " + Chip1.ChipProgMem.ToString + ", " + Chip2.ChipProgMem.ToString)
+			Console.WriteLine(Chip1.ChipName + ": Prog memory differs " + Chip1.ChipProgMem.ToString + ", " + Chip2.ChipProgMem.ToString)
 		End If
 		If Chip1.ChipEEPROM <> Chip2.ChipEEPROM Then
-			Console.WriteLine(Chip1.ChipName + ":EEPROM differs " + Chip1.ChipEEPROM.ToString + ", " + Chip2.ChipEEPROM.ToString)
+			Console.WriteLine(Chip1.ChipName + ": EEPROM differs " + Chip1.ChipEEPROM.ToString + ", " + Chip2.ChipEEPROM.ToString)
 		End If
 		If Chip1.ChipRAM <> Chip2.ChipRAM Then
-			Console.WriteLine(Chip1.ChipName + ":RAM differs " + Chip1.ChipRAM.ToString + ", " + Chip2.ChipRAM.ToString)
+			Console.WriteLine(Chip1.ChipName + ": RAM differs " + Chip1.ChipRAM.ToString + ", " + Chip2.ChipRAM.ToString)
 		End If
 		If Chip1.IO <> Chip2.IO Then
-			Console.WriteLine(Chip1.ChipName + ":IO differs " + Chip1.IO.ToString + ", " + Chip2.IO.ToString)
+			Console.WriteLine(Chip1.ChipName + ": IO differs " + Chip1.IO.ToString + ", " + Chip2.IO.ToString)
 		End If
 		If Chip1.ADCInputs <> Chip2.ADCInputs Then
-			Console.WriteLine(Chip1.ChipName + ":ADC Inputs differs " + Chip1.ADCInputs.ToString + ", " + Chip2.ADCInputs.ToString)
+			Console.WriteLine(Chip1.ChipName + ": ADC Inputs differs " + Chip1.ADCInputs.ToString + ", " + Chip2.ADCInputs.ToString)
 		End If
 		If Chip1.MaxMhz <> Chip2.MaxMhz Then
-			Console.WriteLine(Chip1.ChipName + ":Max Mhz differs " + Chip1.MaxMhz.ToString + ", " + Chip2.MaxMhz.ToString)
+			Console.WriteLine(Chip1.ChipName + ": Max Mhz differs " + Chip1.MaxMhz.ToString + ", " + Chip2.MaxMhz.ToString)
 		End If
 		If Chip1.IntOscMHz <> Chip2.IntOscMHz Then
-			Console.WriteLine(Chip1.ChipName + ":Int Osc MHz differs " + Chip1.IntOscMHz.ToString + ", " + Chip2.IntOscMHz.ToString)
+			Console.WriteLine(Chip1.ChipName + ": Int Osc MHz differs " + Chip1.IntOscMHz.ToString + ", " + Chip2.IntOscMHz.ToString)
 		End If
 		If Chip1.ChipFamily <> Chip2.ChipFamily Then
-			Console.WriteLine(Chip1.ChipName + ":Chip family differs " + Chip1.ChipFamily.ToString + ", " + Chip2.ChipFamily.ToString)
+			Console.WriteLine(Chip1.ChipName + ": Chip family differs " + Chip1.ChipFamily.ToString + ", " + Chip2.ChipFamily.ToString)
 		End If
 		If Chip1.PSP <> Chip2.PSP Then
-			Console.WriteLine(Chip1.ChipName + ":PSP differs " + Chip1.PSP.ToString + ", " + Chip2.PSP.ToString)
+			Console.WriteLine(Chip1.ChipName + ": PSP differs " + Chip1.PSP.ToString + ", " + Chip2.PSP.ToString)
 		End If
 		If Chip1.MaxAddress <> Chip2.MaxAddress Then
-			Console.WriteLine(Chip1.ChipName + ":Max Address differs " + Chip1.MaxAddress.ToString + ", " + Chip2.MaxAddress.ToString)
+			Console.WriteLine(Chip1.ChipName + ": Max Address differs " + Chip1.MaxAddress.ToString + ", " + Chip2.MaxAddress.ToString)
 		End If
 		
 		If Chip1.ConfigWords <> Chip2.ConfigWords Then
-			Console.WriteLine(Chip1.ChipName + ":Config Words differs " + Chip1.ConfigWords.ToString + ", " + Chip2.ConfigWords.ToString)
+			Console.WriteLine(Chip1.ChipName + ": Config Words differs " + Chip1.ConfigWords.ToString + ", " + Chip2.ConfigWords.ToString)
 		End If
 		If Chip1.GPR <> Chip2.GPR Then
-			Console.WriteLine(Chip1.ChipName + ":GPR differs " + Chip1.GPR.ToString + ", " + Chip2.GPR.ToString)
+			Console.WriteLine(Chip1.ChipName + ": GPR differs " + Chip1.GPR.ToString + ", " + Chip2.GPR.ToString)
 		End If
 		If Chip1.HardwareMult <> Chip2.HardwareMult Then
-			Console.WriteLine(Chip1.ChipName + ":Hardware Mult differs " + Chip1.HardwareMult.ToString + ", " + Chip2.HardwareMult.ToString)
+			Console.WriteLine(Chip1.ChipName + ": Hardware Mult differs " + Chip1.HardwareMult.ToString + ", " + Chip2.HardwareMult.ToString)
 		End If
 		
 		'Check interrupts
@@ -139,7 +164,7 @@ Module Program
 		'Console.WriteLine(Chip1.ChipName + ":" + Chip1.InterruptList.Count.ToString + "," + Chip2.InterruptList.Count.ToString + "," + AllInterrupts.Count.ToString)
 		For Each CurrInterrupt As String In AllInterrupts
 			If Not Chip1.InterruptList.ContainsKey(CurrInterrupt) Then
-				Console.WriteLine(Chip1.ChipName + ": Interrupt " + CurrInterrupt + " only in 2")
+				If Not Update Then Console.WriteLine(Chip1.ChipName + ": Interrupt " + CurrInterrupt + " only in 2")
 			Else If Not Chip2.InterruptList.ContainsKey(CurrInterrupt) Then
 				Console.WriteLine(Chip1.ChipName + ": Interrupt " + CurrInterrupt + " only in 1")
 			Else
@@ -164,7 +189,7 @@ Module Program
 		AllRegisters.AddRange(Chip1.SFRVars.Keys.Union(Chip2.SFRVars.Keys))
 		For Each CurrRegister As String In AllRegisters
 			If Not Chip1.SFRVars.ContainsKey(CurrRegister) Then
-				Console.WriteLine(Chip1.ChipName + ": Register " + CurrRegister + " only in 2")
+				If Not Update Then Console.WriteLine(Chip1.ChipName + ": Register " + CurrRegister + " only in 2")
 			Else If Not Chip2.SFRVars.ContainsKey(CurrRegister) Then
 				Console.WriteLine(Chip1.ChipName + ": Register " + CurrRegister + " only in 1")
 			Else
@@ -183,7 +208,7 @@ Module Program
 		AllBits.AddRange(Chip1.SFRVarBits.Keys.Union(Chip2.SFRVarBits.Keys))
 		For Each CurrBit As String In AllBits
 			If Not Chip1.SFRVarBits.ContainsKey(CurrBit) Then
-				Console.WriteLine(Chip1.ChipName + ": Bit " + CurrBit + " only in 2")
+				If Not Update Then Console.WriteLine(Chip1.ChipName + ": Bit " + CurrBit + " only in 2")
 			Else If Not Chip2.SFRVarBits.ContainsKey(CurrBit) Then
 				Console.WriteLine(Chip1.ChipName + ": Bit " + CurrBit + " only in 1")
 			Else
@@ -191,7 +216,16 @@ Module Program
 				Bit1 = Chip1.SFRVarBits(CurrBit)
 				Bit2 = Chip2.SFRVarBits(CurrBit)
 				If Bit1.Var <> Bit2.Var Then
-					Console.WriteLine(Chip1.ChipName + ":" + CurrBit + ": Bit var different " + Bit1.Var + "," + Bit2.Var)
+					'Check location of var. If same, don't show warning
+					Dim LocationDifferent As Boolean = True
+					If Chip2.SFRVars.ContainsKey(Bit1.Var) And Chip2.SFRVars.ContainsKey(Bit2.Var) Then
+						If Chip2.SFRVars(Bit1.Var).Location = Chip2.SFRVars(Bit2.Var).Location Then
+							'All is well
+							LocationDifferent = False
+						End If
+					End If
+					
+					If LocationDifferent Then Console.WriteLine(Chip1.ChipName + ":" + CurrBit + ": Bit var different " + Bit1.Var + "," + Bit2.Var)
 				End If
 				If Bit1.Bit <> Bit2.Bit Then
 					Console.WriteLine(Chip1.ChipName + ":" + CurrBit + ": Bit loc different " + Bit1.Bit.ToString + "," + Bit2.Bit.ToString)
@@ -200,7 +234,110 @@ Module Program
 		Next
 		
 		'Check free ram
+		Dim Banks1, Banks2 As Integer
+		Banks1 = Chip1.FreeRAM.Count
+		Banks2 = Chip2.FreeRAM.Count
+		If Banks1 <> Banks2 Then
+			Console.WriteLine(Chip1.ChipName + ": RAM bank count different")
+		Else
+			For CurrBank = 0 To Banks2 - 1
+				If Chip1.FreeRAM(CurrBank).StartLoc <> Chip1.FreeRAM(CurrBank).StartLoc Or _
+				   Chip1.FreeRAM(CurrBank).EndLoc <> Chip1.FreeRAM(CurrBank).EndLoc Then
+					Console.WriteLine(Chip1.ChipName + ": RAM bank different: " + Chip1.FreeRAM(CurrBank).ToString + "," + Chip2.FreeRAM(CurrBank).ToString)
+				End If
+			Next
+		End If
 		
+		'Check non-banked ram
+		Banks1 = Chip1.NoBankRAM.Count
+		Banks2 = Chip2.NoBankRAM.Count
+		If Banks1 <> Banks2 Then
+			Console.WriteLine(Chip1.ChipName + ": Non-banked RAM bank count different")
+		Else
+			For CurrBank = 0 To Banks2 - 1
+				If Chip1.NoBankRAM(CurrBank).StartLoc <> Chip2.NoBankRAM(CurrBank).StartLoc Or _
+				   Chip1.NoBankRAM(CurrBank).EndLoc <> Chip2.NoBankRAM(CurrBank).EndLoc Then
+					Console.WriteLine(Chip1.ChipName + ": Non-banked RAM bank different: " + Chip1.NoBankRAM(CurrBank).ToString + "," + Chip2.NoBankRAM(CurrBank).ToString)
+				End If
+			Next
+		End If
+		
+		'Check pinouts?
+		
+		'Check default config values
+		Dim AllConfig As New List(Of String)
+		Dim Setting1, Setting2 As String
+		AllConfig.AddRange(Chip1.DefaultConfig.Keys.Union(Chip2.DefaultConfig.Keys))
+		For Each CurrConfig As String In AllConfig
+			If Not Chip1.DefaultConfig.ContainsKey(CurrConfig) Then
+				If Not Update Then Console.WriteLine(Chip1.ChipName + ": Default config " + CurrConfig + " only in 2")
+			Else If Not Chip2.DefaultConfig.ContainsKey(CurrConfig) Then
+				Console.WriteLine(Chip1.ChipName + ": Default config " + CurrConfig + " only in 1")
+			Else
+				'Compare var
+				Setting1 = Chip1.DefaultConfig(CurrConfig)
+				Setting2 = Chip2.DefaultConfig(CurrConfig)
+				If Setting1 <> Setting2 Then
+					Console.WriteLine(Chip1.ChipName + ":" + CurrConfig + ": Default config different " + Setting1 + "," + Setting2)
+				End If
+			End If
+		Next
+		
+		'Check config register masks
+		Dim Masks1, Masks2, CurrMask As Integer
+		Masks1 = Chip1.ConfigMasks.Count
+		Masks2 = Chip2.ConfigMasks.Count
+		If Masks1 <> Masks2 Then
+			Console.WriteLine(Chip1.ChipName + ": Default mask count different")
+		Else
+			For CurrMask = 0 To Masks2 - 1
+				If Chip1.ConfigMasks(CurrMask) <> Chip2.ConfigMasks(CurrMask) Then
+					Console.WriteLine(Chip1.ChipName + ": Default mask different: " + Chip1.ConfigMasks(CurrMask).ToString + "," + Chip2.ConfigMasks(CurrMask).ToString)
+				End If
+			Next
+		End If
+		
+		'Check config options
+		AllConfig.Clear
+		AllConfig.AddRange(Chip1.ConfigSettings.Keys.Union(Chip2.ConfigSettings.Keys))
+		Dim Options1, Options2 As List(Of String)
+		For Each CurrConfig As String In AllConfig
+			If Not Chip1.DefaultConfig.ContainsKey(CurrConfig) Then
+				If Not Update Then Console.WriteLine(Chip1.ChipName + ": Config setting " + CurrConfig + " only in 2")
+			Else If Not Chip2.DefaultConfig.ContainsKey(CurrConfig) Then
+				Console.WriteLine(Chip1.ChipName + ": Config setting " + CurrConfig + " only in 1")
+			Else
+				'Compare var
+				Options1 = Chip1.ConfigSettings(CurrConfig)
+				Options2 = Chip2.ConfigSettings(CurrConfig)
+				For Each CurrOption As String In Options1.Union(Options2)
+					If Not Options1.Contains(CurrOption) Then
+						If Not Update Then Console.WriteLine(Chip1.ChipName + ":" + CurrConfig + ": Config setting option " + CurrOption + " only in 2")
+					Else If Not Options2.Contains(CurrOption) Then
+						Console.WriteLine(Chip1.ChipName + ":" + CurrConfig + ": Config setting option " + CurrOption + " only in 1")
+					End If
+				Next
+			End If
+		Next
+		
+		'Check config option masks
+		AllConfig.Clear
+		AllConfig.AddRange(Chip1.ConfigBitMasks.Keys.Union(Chip2.ConfigBitMasks.Keys))
+		Dim BitMask1, BitMask2 As ChipConfigBitMask
+		For Each CurrConfig As String In AllConfig
+			If Not Chip1.ConfigBitMasks.ContainsKey(CurrConfig) Then
+				If Not Update Then Console.WriteLine(Chip1.ChipName + ": Config mask " + CurrConfig + " only in 2")
+			Else If Not Chip2.ConfigBitMasks.ContainsKey(CurrConfig) Then
+				Console.WriteLine(Chip1.ChipName + ": Config mask " + CurrConfig + " only in 1")
+			Else
+				'Compare var
+				BitMask1 = Chip1.ConfigBitMasks(CurrConfig)
+				BitMask2 = Chip2.ConfigBitMasks(CurrConfig)
+				If Not BitMask1.Equals(BitMask2) Then
+					Console.WriteLine(Chip1.ChipName + ":" + CurrConfig + ": Config mask different " + BitMask1.ToString + "," + BitMask2.ToString)
+				End If
+			End If
+		Next
 		
 	End Sub
 	
