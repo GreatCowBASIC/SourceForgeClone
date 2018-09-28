946e315c6e5db40ff5c7af946bc85b2a10ac41d0
Anobium <17933379+Anobium@users.noreply.github.com>
2018-09-28 07:02:37 +0000

    Darwin Adaptations and Scripts for DarwinBuild

--- GCBASIC/trunk/DarwinBuild/build.sh
+++ GCBASIC/trunk/DarwinBuild/build.sh
@@ -4,32 +4,35 @@ echo
 echo "Compiling Great Cow BASIC for macOS with FreeBASIC"
 echo
 
-# You need to install GNU's gcc compiler
+# You need to install the GNU gcc compiler
 #
 # Failure to use the gcc compiler will result in the following multiple
 # errors from Apple's clang compiler:
 # error: indirect goto in function with no address-of-label expressions
 #
-# Correct path below for your gcc version and location
+# Correct the path below for your gcc version and location
 export PATH=/usr/local/gcc-8.1/bin:$PATH
 
 # You need to install Xcode from the Apple App Store
 #
-# Set the library path for your Xcode version
+# Set the SDK library path for your Xcode version
 export LIBRARY_PATH=/Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX10.14.sdk/usr/lib
 
-# Set theinclude paths for your Xcode version
+# Set the SDK include paths for your Xcode version
 export C_INCLUDE_PATH=/Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX10.14.sdk/usr/include
 
 # FreeBASIC has a hardcoded OS target version; this needs to match below
-# or you will get the following linker error:
-# ld: warning: object file (gcbasic.o) was built for newer OSX version (10.13) than being linked (10.4)
-export MACOSX_DEPLOYMENT_TARGET=10.4
+# or you will get a linker error similar to the following:
+# ld: warning: object file (gcbasic.o) was built for newer OSX version (10.13) than being linked (10.1)
+export MACOSX_DEPLOYMENT_TARGET=10.1
+
+# Change to the Great Cow BASIC source directory
+cd ~/GreatCowBasic/Sources
 
 # Now compile!
 #
-# Adjust paths to your FreeBASIC version and location
-~/Documents/fbc-1.06/bin/fbc -exx -v -arch native gcbasic.bas
+# Correct the path for your FreeBASIC version and location
+~/fbc-1.06/bin/fbc -exx -v -arch native gcbasic.bas
 
 echo
 echo "Finished"
---
+++ GCBASIC/trunk/DarwinBuild/readme.rd
@@ -0,0 +1,6 @@
+Folder contains shell script pkg2dmg.sh which is the build 
+script to take the Packages 
+(http://s.sudre.free.fr/Software/Packages/about.html) generated .pkg 
+file installation for macOS and create the compressed disk image (.dmg)  file. 
+
+This is not an enduser file.
